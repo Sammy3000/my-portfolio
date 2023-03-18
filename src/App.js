510eccc81704c99e6e03148a4de43a0d8a585b14
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-03-18 10:25:58 +0300

    Create functional components for all routes and import them in App js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './index.css';
+import Home from './routes/Home';
+import Projects from './routes/Projects ';
+import About from './routes/About';
+import Contact from './routes/Contact';
 
 function App() {
   return (
