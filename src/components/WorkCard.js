743754267e2f5d4f2d5451bcc5ea720a261ffa38
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-05-18 22:58:12 +0300

    Initialize class names for projects container

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -2,7 +2,14 @@ import './WorkCard.css';
 import React from 'react';
 
 const WorkCard = () => {
-  return <div>WorkCard</div>;
+  return (
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card"></div>
+      </div>
+    </div>
+  );
 };
 
 export default WorkCard;
