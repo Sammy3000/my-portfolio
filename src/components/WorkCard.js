9f390317b51410b0db49f0a3dad1292926ebd21e
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-05-19 09:29:20 +0300

    Add project image

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -1,12 +1,18 @@
 import './WorkCard.css';
 import React from 'react';
+import proj1 from '../assets/coding.jpg';
 
 const WorkCard = () => {
   return (
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
-        <div className="project-card"></div>
+        <div className="project-card">
+          <img
+            src={proj1}
+            alt="project pic"
+          />
+        </div>
       </div>
     </div>
   );
