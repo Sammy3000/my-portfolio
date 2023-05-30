48e39cf2842ecae9414f1518860797d91da88066
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-05-30 17:53:55 +0300

    Modify projects jsx

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div>
+      <h2>Projects container</h2>
+    </div>
+  );
 };
 
 export default Work;
