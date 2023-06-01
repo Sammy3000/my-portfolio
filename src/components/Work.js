02e050c8fd11a4f42826d2da61c4f21328dd5faf
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-06-01 10:28:47 +0300

    Create a single project content

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,38 @@
+import './WorkCard.css';
 import React from 'react';
+import proj1 from '../assets/coding.jpg';
+import { NavLink } from 'react-router-dom';
 
 const Work = () => {
   return (
-    <div>
-      <h2>Projects container</h2>
+    <div className="work-container">
+      <h1 className="project-heading">Projects</h1>
+      <div className="project-container">
+        <div className="project-card">
+          <img
+            src={proj1}
+            alt="project pic"
+          />
+          <h2 className="project-title">Project Title</h2>
+          <div className="project-details">
+            <p> This is a description of my project</p>
+            <div className="pro-buttons">
+              <NavLink
+                to="towettsammy.com"
+                className="btn"
+              >
+                View
+              </NavLink>
+              <NavLink
+                to="towettsammy.com"
+                className="btn"
+              >
+                Source
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
