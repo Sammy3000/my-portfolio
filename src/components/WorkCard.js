5c1961ad36e1073ca8dc048ca7e3a661d251e8b8
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-06-06 11:09:04 +0300

    Pass projects dataset to workcard file

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -3,31 +3,31 @@ import React from 'react';
 import proj1 from '../assets/coding.jpg';
 import { NavLink } from 'react-router-dom';
 
-const WorkCard = () => {
+const WorkCard = (props) => {
   return (
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
         <div className="project-card">
           <img
-            src={proj1}
+            src={props.imgsrc}
             alt="project pic"
           />
-          <h2 className="project-title">Project Title</h2>
+          <h2 className="project-title">{props.heading}</h2>
           <div className="project-details">
-            <p> This is a description of my project</p>
+            <p>{props.description}</p>
             <div className="pro-buttons">
               <NavLink
                 to="towettsammy.com"
                 className="btn"
               >
-                View
+                {props.view}
               </NavLink>
               <NavLink
                 to="towettsammy.com"
                 className="btn"
               >
-                Source
+                {props.source}
               </NavLink>
             </div>
           </div>
