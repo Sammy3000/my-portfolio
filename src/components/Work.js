e61b0ef96f422ba3d16eb40a75efcca891acd022
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-06-06 11:11:56 +0300

    Modify work file

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,30 +8,7 @@ const Work = () => {
     <div className="work-container">
       <h1 className="project-heading">Projects</h1>
       <div className="project-container">
-        <div className="project-card">
-          <img
-            src={proj1}
-            alt="project pic"
-          />
-          <h2 className="project-title">Project Title</h2>
-          <div className="project-details">
-            <p> This is a description of my project</p>
-            <div className="pro-buttons">
-              <NavLink
-                to="towettsammy.com"
-                className="btn"
-              >
-                View
-              </NavLink>
-              <NavLink
-                to="towettsammy.com"
-                className="btn"
-              >
-                Source
-              </NavLink>
-            </div>
-          </div>
-        </div>
+        <div className="project-card"></div>
       </div>
     </div>
   );
