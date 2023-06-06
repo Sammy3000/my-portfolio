e8c387367b2aa308b966c7dc07aeb06f9aa8724c
Sammy Towett <77583216+Sammy3000@users.noreply.github.com>
2023-06-06 10:54:37 +0300

    Create an object variable for image,heading and description for our data

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,4 +1,10 @@
 import project1 from '../assets/codex.jpg';
 import project2 from '../assets/codex2.jpg';
 import project3 from '../assets/coding.jpg';
-const projectCardData = [];
+const projectCardData = [
+  {
+    imgsrc: project1,
+    heading: 'Bookstore',
+    description: 'Bookstore project designed to store books for user.',
+  },
+];
